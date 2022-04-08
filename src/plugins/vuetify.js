db58020d9daa3dc2bc9fc5e99335760b13526261
Enzo Notario <enzo.notario@gmail.com>
2022-04-07 23:54:03 -0300

    Update color && default code

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,7 +12,7 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: '#007BFF',
+        primary: '#006EB3',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
