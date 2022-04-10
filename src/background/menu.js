dc38d06618a3b069f41871520502b52cd275aea4
Melvin_Guerrero <melvin_guerrero@yahoo.com>
2022-04-10 03:08:34 -0400

    ventana About App

--- src/background/menu.js
+++ src/background/menu.js
@@ -1,4 +1,7 @@
 import { Menu } from 'electron';
+import path, { join } from 'path';
+
+const openAboutWindow = require('electron-about-window').default;
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
@@ -111,7 +114,7 @@ function createMenu() {
         { type: 'separator' },
         { label: 'Pantalla completa', role: 'togglefullscreen' }
       ]
-  },
+    },
 
     {
       label: 'Ventana',
@@ -169,13 +172,38 @@ function createMenu() {
 
         { type: 'separator' },
 
+        // {
+        //   label: 'Acerca del programa',
+        //   role: 'help',
+        //   click: async () => {
+        //     const { shell } = require('electron');
+        //     await shell.openExternal('https://latinoeditor.enzonotario.me');
+        //   },
         {
           label: 'Acerca del programa',
-          role: 'help',
-          click: async () => {
-            const { shell } = require('electron');
-            await shell.openExternal('https://latinoeditor.enzonotario.me');
-          },
+          click: (item, focusedWindow) =>
+            openAboutWindow({
+              icon_path: join(__static, 'icon.png'),
+              description: 'Editor de texto para el Lenguaje Latino',
+              copyright: 'Copyleft (c) 2022, Lenguaje Latino',
+              package_json_dir: __dirname,
+              bug_report_url: 'https://github.com/lenguaje-latino/latino-editor/issues',
+              homepage: 'https://github.com/lenguaje-latino/latino-editor',
+              license: 'MIT',
+              win_options: {
+                parent: focusedWindow,
+                modal: true,
+                width: 300,
+                height: 450,
+                center: true,
+                resizable: false,
+                minimizable: false,
+                maximizable: false,
+                alwaysOnTop: true,
+              },
+              use_version_info: true,
+              show_close_button: 'Cerrar',
+            }),
         },
       ],
     },
